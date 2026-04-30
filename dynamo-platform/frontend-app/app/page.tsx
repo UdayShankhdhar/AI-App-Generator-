72b85a606ac7e82761d45234853b69036ee240ff
UdayShankhdhar <shankhdharuday2@gmail.com>
2026-04-30 23:00:43 +0530

    Update page.tsx

--- dynamo-platform/frontend-app/app/page.tsx
+++ dynamo-platform/frontend-app/app/page.tsx
@@ -1,18 +1,5 @@
-import Link from "next/link";
-import { config } from "../lib/config";
+import { redirect } from "next/navigation";
 
 export default function Home() {
-  return (
-    <div>
-      <h1>{config.app.name}</h1>
-
-      <ul>
-        {config.pages.map((page) => (
-          <li key={page.id}>
-            <Link href={`/${page.id}`}>{page.title}</Link>
-          </li>
-        ))}
-      </ul>
-    </div>
-  );
+  redirect("/login");
 }
