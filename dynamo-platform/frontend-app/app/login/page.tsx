2b8e619586572c8e591c9b3e30198405feee9730
UdayShankhdhar <shankhdharuday2@gmail.com>
2026-05-04 14:38:37 +0530

    Update page.tsx

--- dynamo-platform/frontend-app/app/login/page.tsx
+++ dynamo-platform/frontend-app/app/login/page.tsx
@@ -1,20 +1,50 @@
 "use client";
 
 import { useEffect, useState } from "react";
+import { useRouter } from "next/navigation";
 
 export default function LoginPage() {
+  const router = useRouter();
 
-  const [config, setConfig] =
-    useState<any>(null);
+  const [config, setConfig] = useState<any>(null);
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
   useEffect(() => {
-
     const API = process.env.NEXT_PUBLIC_API_URL;
+
     fetch(`${API}/api/config`)
       .then((res) => res.json())
       .then(setConfig);
-
   }, []);
+
+  async function handleLogin() {
+    const API = process.env.NEXT_PUBLIC_API_URL;
+
+    const res = await fetch(`${API}/api/login`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        email,
+        password,
+      }),
+    });
+
+    const data = await res.json();
+
+    if (data.token) {
+      // SAVE TOKEN
+      localStorage.setItem("token", data.token);
+
+      // redirect
+      router.push("/dashboard");
+    } else {
+      alert("Invalid credentials");
+    }
+  }
 
   if (!config) {
     return <div>Loading...</div>;
@@ -27,17 +57,16 @@ export default function LoginPage() {
         margin: "100px auto",
       }}
     >
+      <h1>{config.auth.ui.title}</h1>
 
-      <h1>
-        {config.auth.ui.title}
-      </h1>
-
-      <p>
-        {config.auth.ui.subtitle}
-      </p>
+      <p>{config.auth.ui.subtitle}</p>
 
       <input
         placeholder="Email"
+        value={email}
+        onChange={(e) =>
+          setEmail(e.target.value)
+        }
       />
 
       <br />
@@ -46,15 +75,18 @@ export default function LoginPage() {
       <input
         placeholder="Password"
         type="password"
+        value={password}
+        onChange={(e) =>
+          setPassword(e.target.value)
+        }
       />
 
       <br />
       <br />
 
-      <button>
+      <button onClick={handleLogin}>
         Login
       </button>
-
     </div>
   );
 }
